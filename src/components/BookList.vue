7925c0e9dc8003f0ba4ef14cfea34cd28658db02
Sanjithkumar V <sanjith@access-designs.com>
2018-10-03 10:13:02 +0000

    Completed the module

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -1,13 +1,26 @@
 <template>
-  <div><h1>{{title}}</h1></div>
+  <div>
+    <h1>{{title}}</h1>
+    <ul>
+      <li v-for="book in books">
+        {{book.title}}:{{book.author}}
+      </li>
+    </ul>
+  </div>
+
 
 </template>
 <script>
-  export default{
+  export default {
     name:"BookList",
     data:function(){
       return {
-        title:"All Books"
+        title:"All Books",
+        books:[
+          {title: 'Self-Reliance', author: 'Ralph Waldo Emerson'},
+          {title: 'American Gods', author: 'Neil Gaiman'},
+          {title: 'Amusing Ourselves to Death', author: 'Neil Postman'},
+        ]
       }
     }
   }
@@ -15,5 +28,13 @@
 <style>
   h1,h2 {
     font-weight: normal;
+  }
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+  li{
+    display: block;
+    margin: 0 10px;
   }
 </style>
