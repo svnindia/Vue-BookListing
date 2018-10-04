b4d506a302331bfc2884efd58022721a01218ea9
Sanjithkumar V <sanjith@access-designs.com>
2018-10-04 07:14:23 +0000

    Module Completed

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -4,14 +4,18 @@
     <ul>
         <book-item v-for="book in books" :book="book" :key="book.title"></book-item>
     </ul>
+    <hr>
+    <book-form @addBook="appendBook"></book-form>
   </div>
 </template>
 <script>
   import BookItem from './BookItem';
+  import BookForm from './BookForm';
   export default {
     name:"BookList",
     components:{
-      BookItem
+      BookItem,
+      BookForm
     },
     data:function(){
       return {
@@ -21,6 +25,11 @@
           {title: 'American Gods', author: 'Neil Gaiman'},
           {title: 'Amusing Ourselves to Death', author: 'Neil Postman'},
         ]
+      }
+    },
+    methods:{
+      appendBook(bookTitle,bookAuthor){
+        this.books.push({title:bookTitle,author:bookAuthor})
       }
     }
   }
