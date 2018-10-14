9039e087a2db78e8bb62a565294acfd5af22aaa0
Sharat005 <sharatnaik1996@gmail.com>
2018-10-14 20:31:19 +0530

    Module 4

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -2,7 +2,7 @@
 	 <div>
 	 	<h1>{{ title }}</h1>
 	 	<ul>
-	 		<li v-for='book in books'> {{book.title}} : {{book.author}} </li>
+	 		<book-item v-for="book in books" :book='book'> </book-item>
 	 	</ul>
 	</div>
 
@@ -10,8 +10,13 @@
 
 <script>
 
+import BookItem from './BookItem';
+
 export default {
     name: 'BookList',
+    components: {
+      BookItem
+    },
 	data() {
       return {
         title: 'All Books',
