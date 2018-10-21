5bc31924cd220be89f1076746f9a723adc94010e
vedahm <vedhm2cs@gmail.com>
2018-10-21 22:54:48 +0530

    module 4

--- src/components/bookList.vue
+++ src/components/bookList.vue
@@ -1,41 +1,43 @@
 <template>
-<div>
-  <h1>{{title}}</h1>
-   <ul>
-      <li v-for='book in books'>{{book.title}}:{{book.author}}</li>
-   </ul>
-</div>
-
-
+  <div>
+    <h1>{{title}}</h1>
+    <ul>
+      <book-item v-for='book in books' :book="book"></book-item>
+    </ul>
+  </div>
 </template>
 
 <script>
-export default {
-  name: 'Booklist',
-  data(){
-   return {
-    title :'All vedaa',
-    books :[
-      {title: "kannada", author:'kuvempu'},
-      {title: "English", author: "David champel"},
-      {title: "MAthematics", author: "aryabhatta"},
-    ]
-   }
+  import bookItem from './bookItem';
+  export default {
+    name: 'Booklist',
+    components : {
+      BookItem
+    },
+    data(){
+      return {
+        title :'All vedaa',
+        books :[
+          {title: "kannada", author:'kuvempu'},
+          {title: "English", author: "David champel"},
+          {title: "MAthematics", author: "aryabhatta"},
+        ]
+      }
+    }
   }
-}
 
 </script>
 
 <style>
-h1, h2 {
-  font-weight: normal
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li{
-  display:block;
-  margin:0 10px;
-}
+  h1, h2 {
+    font-weight: normal
+  }
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+  li {
+    display:block;
+    margin:0 10px;
+  }
 </style>
