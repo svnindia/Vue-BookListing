acf28be6a5ceb43301cb187797be3d04d22e273c
vedahm <vedhm2cs@gmail.com>
2018-10-21 22:09:28 +0530

    adding the components

---
+++ src/components/bookList.vue
@@ -0,0 +1,25 @@
+<template>
+<div>
+  <h1>{{title}}</h1>
+</div>
+
+
+</template>
+
+<script>
+export default {
+  name: 'Booklist',
+  data(){
+   return {
+    title :'All vedaa'
+   }
+  }
+}
+
+</script>
+
+<style>
+h1, h2 {
+  font-weight: normal
+}
+</style>
