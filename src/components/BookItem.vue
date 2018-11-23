662549c8bfad494c84c7b5d9ac69685d88adc0b4
Rahul <sharma.rahul2509@gmail.com>
2018-11-23 13:47:14 +0530

    book list in vuejs

---
+++ src/components/BookItem.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <li>{{book.title}} : {{book.author}}</li>
+  </div>
+
+</template>
+
+<script>
+  export default {
+    name: 'BookItem',
+    props:['book']
+  };
+</script>
+
+<style >
+
+
+  li{
+    display:block;
+  }
+</style>
---
+++ src/components/BookList.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <h1>
+      {{title}}
+    </h1>
+    <ul>
+      <book-item v-for="book in books" :book="book"></book-item>
+    </ul>
+    <hr>
+    <book-form @addBook="appendBook"></book-form>
+  </div>
+
+</template>
+
+<script>
+  import BookItem from './BookItem.vue'
+  import BookForm from './BookForm.vue'
+  export default {
+    name: 'BookList',
+    components:{
+        BookItem,
+        BookForm
+    },
+    data() {
+      return {
+        title: 'All Books',
+        books:[
+          {title:'Let Us C',author:'yashwant Kanetkar'},
+          {title:'Concept of Physics',author:'H C Verma'},
+          {title:'Mathematics By RD Sharma',author:'R D Sharma'}
+        ]
+      };
+    },
+    methods:{
+      appendBook(at,au){
+        this.books.push({title:at,author:au});
+      }
+    }
+  };
+</script>
+
+<style >
+  h1,h2 {
+    font-weight:normal;
+  }
+
+  ul{
+    list-style-type:none;
+    padding:0
+  }
+
+</style>
